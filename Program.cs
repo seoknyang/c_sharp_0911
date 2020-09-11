f525d1ff5d3195b65c8d2b1aa8364a1362671689
seoknyang <55523889+seoknyang@users.noreply.github.com>
2020-09-11 10:28:04 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("학번:201787002");
+            Console.WriteLine("이름:천석녕");
+        }
+    }
+}
